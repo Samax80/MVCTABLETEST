ce467f5630df5121ff5c75febb95fa1778565976
Samax80 <47028750+Samax80@users.noreply.github.com>
2019-11-05 14:04:35 -0500

    Add files via upload
    This is a back Up from the project (rout config,controller,model, and view) only

---
+++ Controllers/MatrixController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SibiliTest.Models;//we include this in order to have acces to the member variables of MatrixModel Class
+
+namespace SibiliTest.Controllers
+{
+    public class MatrixController : Controller
+    {
+        
+        public ActionResult GetMatrixDetails()
+        {
+           
+            return View();
+        }
+
+        public ActionResult SetMatrixDetails(int Xdimention, int Ydimention, int? FormatNumber)
+        /*this Function will receive the 3 arguments (int Xdimention, int Ydimention, int? FormatNumber)  fromm the view  GetMatrixDetails.cshtml
+          to be set at the SetMatrixDetails.cshtml (
+         Note:'int?' means that the FormatNumber is null able: FormatNumber can be--> int? FormatNumber=null is ok; !! if is not used--> int FormatNumber=null not ok*/
+        {
+            MatrixModel matrixObj = new MatrixModel();//MatrixModel object that will be passed to the SetMatrixDetails.cshtml view in order to display the values on it!
+
+
+            if (FormatNumber == null)//if the formatNumber is Null (not selected) will set the value to 0
+            {
+                matrixObj.FormatNumber = 0;
+            }
+
+            matrixObj.Xdimention = Xdimention;//Passing the value to the MatrixModel object "matrixObj"
+
+            matrixObj.Ydimention = Ydimention;//Passing the value to the MatrixModel object "matrixObj"
+
+            matrixObj.FormatNumber = Convert.ToInt32(FormatNumber);//Implicity convertion is not possible ! int?  to int! ,cast( Convert.ToInt32)  is used instead
+
+            return View(matrixObj);//Passing the MatrixModel object  to the SetMatrixDetails.cshtml view 
+
+        }
+
+    }
+}
---
+++ Models/MatrixModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SibiliTest.Models
+{
+    public class MatrixModel
+    {
+
+                
+        public int Xdimention { get; set; }
+        public int Ydimention { get; set; }
+        public string color { get; set; }
+        public string font { get; set; }
+        public int FormatNumber { get; set; }//will hold the selected format (Binary,Hex, or Decimal)
+
+
+        /*This functions will be  used at the  SetMatrixDetails.cshtml view to convert the number to the selected format */
+        public string ToBinary(int MyNumber)
+        {
+            Int64 myDecimal = Convert.ToInt64(MyNumber);
+
+            Int64 BinaryHolder;
+            char[] BinaryArray;
+            string BinaryResult = "";
+
+            while (myDecimal > 0)
+            {
+                BinaryHolder = myDecimal % 2;
+                BinaryResult += BinaryHolder;
+                myDecimal = myDecimal / 2;
+            }
+
+            BinaryArray = BinaryResult.ToCharArray();
+            Array.Reverse(BinaryArray);
+            BinaryResult = new string(BinaryArray);
+
+            return BinaryResult;
+
+        }/**/
+
+        public string ToHex(int MyNumber)
+
+        {
+
+            string hexValue = MyNumber.ToString("X");
+            
+            return hexValue;
+        }
+
+        public Decimal ToDecimal(int MyNumber)
+
+        {
+
+            decimal decimalvalue = (Convert.ToDecimal(MyNumber));
+
+            return decimalvalue;
+
+        }
+
+
+        /*
+         FUNCTION TO BE IMPLEMENTED FOR :(Optional) – color the background of the Prime numbers.
+          public static bool IsPrime(int number)
+{
+    if (number <= 1) return false;
+    if (number == 2) return true;
+    if (number % 2 == 0) return false;
+
+    var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+    for (int i = 3; i <= boundary; i+=2)
+        if (number % i == 0)
+            return false;
+
+    return true;        
+}*/
+
+
+    }
+}
